fde1a09724626192a82b620b3c2e2ccad9f9d4f3
Sulistyo Harjo <sulistyoharjo@gmail.com>
2025-12-24 12:47:48 +0700

    Extracted feed item

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,55 @@ Route::get('/', function () {
 });
 
 Route::get('/feed', function () {
-    return view('feed');
+    $feedItems = json_decode(
+        json_encode([
+            [
+                'postedDateTime' => '3h',
+                'content' => <<<str
+                <p>
+                    I made this. <a href="#">#myartwork</a> <a href="#">#pixl</a>
+                </p>
+                <img src="/images/simon-chilling.png" alt="" />
+                str,
+                'likeCount' => 23,
+                'replyCount' => 45,
+                'repostCount' => 151,
+                'profile' => [
+                    'displayName' => 'Michael',
+                    'handle' => '@mich_jj',
+                    'avatar' => '/images/michael.png'
+                ],
+            ]
+        ])
+    );
+
+
+    return view('feed', compact('feedItems'));
 });
 
 Route::get('/profile', function () {
-    return view('profile');
+    $feedItems = json_decode(
+        json_encode([
+            [
+                'postedDateTime' => '3h',
+                'content' => <<<str
+                <p>
+                    I made this. <a href="#">#myartwork</a> <a href="#">#pixl</a>
+                </p>
+                <img src="/images/simon-chilling.png" alt="" />
+                str,
+                'likeCount' => 23,
+                'replyCount' => 45,
+                'repostCount' => 151,
+                'profile' => [
+                    'displayName' => 'Michael',
+                    'handle' => '@mich_jj',
+                    'avatar' => '/images/michael.png'
+                ],
+            ]
+        ])
+    );
+
+
+    return view('profile', compact('feedItems'));
 });
